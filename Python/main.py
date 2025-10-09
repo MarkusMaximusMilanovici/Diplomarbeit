7a7a67e6c0fb62250c1be6182cfe4255d43cee0f
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2025-10-09 10:11:20 +0200

    test

--- Python/main.py
+++ Python/main.py
@@ -2,7 +2,7 @@ from picamera2 import Picamera2
 import cv2
 
 picam2 = Picamera2()
-picam2.preview_configuration.main.size = (1280, 720)
+picam2.preview_configuration.main.size = (720, 1280)
 picam2.preview_configuration.main.format = "RGB888"
 picam2.configure("preview")
 picam2.start()
