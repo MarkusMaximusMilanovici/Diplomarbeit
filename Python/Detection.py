aada5abc735a861a8ee52645f9dd237404097b7e
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2025-10-15 16:05:40 +0200

    Test File for Detection

--- Python/Detection.py
+++ Python/Detection.py
@@ -2,7 +2,7 @@ from picamera2 import Picamera2
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
 vcap = cv2.VideoCapture()
 
