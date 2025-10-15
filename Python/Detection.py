46fc9232d138b00ebf9a440ff3bb2066725b4768
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2025-10-15 15:42:02 +0200

    Test File for Detection

--- Python/Detection.py
+++ Python/Detection.py
@@ -19,7 +19,10 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    print(gray)
+
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
     for (x,y,w,h) in faces:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
 
