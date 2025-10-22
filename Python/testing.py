8b9f428b0f3e05ef10a945bed8a081b56028e30d
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2025-10-22 15:56:01 +0200

    Test File for Detection

--- Python/testing.py
+++ Python/testing.py
@@ -1,8 +1,20 @@
-import spidev
+import spidev, time
+
 spi = spidev.SpiDev()
 spi.open(10, 0)
 spi.max_speed_hz = 1000000
-print("Sending test bytes...")
-spi.xfer2([0xAA, 0x55, 0xFF, 0x00])
-print("Done.")
+
+def send_cmd(reg, data):
+    spi.xfer2([reg, data] * 4)
+
+def init():
+    send_cmd(0x09, 0x00)
+    send_cmd(0x0A, 0x0F)
+    send_cmd(0x0B, 0x07)
+    send_cmd(0x0C, 0x01)
+    send_cmd(0x0F, 0x01)  # TEST MODE ON
+    time.sleep(3)
+    send_cmd(0x0F, 0x00)  # TEST MODE OFF
+
+init()
 spi.close()
