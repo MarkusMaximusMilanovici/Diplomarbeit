72498a2fea61b5812fee6977e5a33f19aad1dfd1
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2025-10-22 16:18:30 +0200

    Test File for Detection

--- Python/testing.py
+++ Python/testing.py
@@ -4,10 +4,10 @@ spi = spidev.SpiDev()
 spi.open(10, 0)
 spi.max_speed_hz = 1000000
 
-spi.xfer2([0xC, 1])
 spi.xfer2([0x9, 0])
 spi.xfer2([0xA, 0x7])
 spi.xfer2([0xB, 0xFF])
+spi.xfer2([0xC, 1])
 spi.xfer2([0x1, 0xFF])
 time.sleep(3)
 spi.xfer2([0xC, 0])
