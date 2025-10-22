6c95d90a91d272fb929d582e8513adf81e9b76ba
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2025-10-22 16:28:49 +0200

    Test File for Detection

--- Python/testing.py
+++ Python/testing.py
@@ -5,21 +5,21 @@ spi.open(10, 0)
 spi.max_speed_hz = 1000000
 
 def noop():
-    spi.xfer2([0x0, 0] * 3)  # Decode 0
+    spi.xfer3([0x0, 0] * 3)  # Decode 0
 
-spi.xfer2([0x9, 0]) # Decode 0
+spi.xfer3([0x9, 0]) # Decode 0
 noop()
-spi.xfer2([0xA, 0x7]) # Intensity 15/32
+spi.xfer3([0xA, 0x7]) # Intensity 15/32
 noop()
-spi.xfer2([0xB, 0x7]) # Scan limit 8 digits
+spi.xfer3([0xB, 0x7]) # Scan limit 8 digits
 noop()
-spi.xfer2([0xC, 1]) # Shutdown Normal Operation
+spi.xfer3([0xC, 1]) # Shutdown Normal Operation
 noop()
-spi.xfer2([0xF, 1]) # Display Test On
+spi.xfer3([0xF, 1]) # Display Test On
 noop()
 time.sleep(3)
-spi.xfer2([0xF, 0]) # Display Test Off
+spi.xfer3([0xF, 0]) # Display Test Off
 noop()
-spi.xfer2([0xC, 0]) # Shutdown
+spi.xfer3([0xC, 0]) # Shutdown
 noop()
 spi.close()
