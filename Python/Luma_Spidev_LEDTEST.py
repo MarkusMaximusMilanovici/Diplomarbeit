d5ed9ea7a3b501a89a243e4d261bdbbd4a4c0839
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2025-10-23 11:13:05 +0200

    Test File for Detection

--- Python/Luma_Spidev_LEDTEST.py
+++ Python/Luma_Spidev_LEDTEST.py
@@ -1,13 +1,14 @@
-#sudo apt install python3-pip
-#pip3 install luma.led_matrix
-
-from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
+from luma.led_matrix.device import max7219
 from luma.core.render import canvas
-from PIL import ImageDraw
+import time
 
-serial = spi(port=10, device=0, gpio=noop())
-device = max7219(serial, cascaded=4, block_orientation=90, rotate=0)
+serial = spi(port=0, device=0, gpio=noop())  # <-- use the classic one
+device = max7219(serial, cascaded=4)
+device.contrast(255)
 
 with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="white", fill="white")
+    draw.rectangle(device.bounding_box, fill="white")
+
+time.sleep(3)
+device.clear()
