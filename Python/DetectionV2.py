6c2385132e2d598de0496235a422f29531cafd7d
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2025-11-13 09:59:08 +0100

    Test File for Detection

--- Python/DetectionV2.py
+++ Python/DetectionV2.py
@@ -35,4 +35,27 @@ while True:
     _, fgmask = cv2.threshold(fgmask, 127, 255, cv2.THRESH_BINARY)
 
     contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    mask_filled = np.zeros_like
+    mask_filled = np.zeros_like(fgmask)
+    min_area = 150        # Toleranz für kleine Konturen erhöht
+    area_sum = 0
+    for c in contours:
+        if cv2.contourArea(c) > min_area:
+            cv2.drawContours(mask_filled, [c], -1, 255, -1)
+            area_sum += cv2.contourArea(c)
+    active_person_found = area_sum > 350   # Gesamtfläche für „aktiv“ deutlich niedriger
+
+    mask_filled = cv2.medianBlur(mask_filled, 3)
+
+    # --- Zeige letzte Maske, falls gerade keine erkannt ---
+    if active_person_found:
+        last_mask = mask_filled.copy()
+    elif last_mask is not None:
+        mask_filled = last_mask.copy()
+
+    cv2.imshow("Person Mask", mask_filled)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.stop()
+cv2.destroyAllWindows()
