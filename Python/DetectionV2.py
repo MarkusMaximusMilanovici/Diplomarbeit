c3cd799f89b232073a6653a6e891b1b7c0ca683f
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2025-11-13 11:09:15 +0100

    Test File for Detection

--- Python/DetectionV2.py
+++ Python/DetectionV2.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from picamera2 import Picamera2
 
+# --- Initialize camera ---
 cam = Picamera2()
 config = cam.create_preview_configuration(main={"size": (1280, 720)})
 cam.configure(config)
@@ -11,12 +12,10 @@ fgbg = cv2.createBackgroundSubtractorKNN(history=70, dist2Threshold=400.0, detec
 
 kernel_erode = np.ones((3, 3), np.uint8)
 kernel_dilate = np.ones((7, 7), np.uint8)
-kernel_close = np.ones((12, 12), np.uint8)
+kernel_close = np.ones((7, 7), np.uint8)
 
-min_area = 80
-active_area_sum = 120
-
-last_mask = None
+master_mask = None
+max_area_sum = 0
 
 while True:
     frame = cam.capture_array()
@@ -30,7 +29,6 @@ while True:
     gray = cv2.equalizeHist(gray)
 
     fgmask = fgbg.apply(gray)
-
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel_close)
     fgmask = cv2.dilate(fgmask, kernel_dilate, iterations=2)
     fgmask = cv2.erode(fgmask, kernel_erode, iterations=1)
@@ -39,34 +37,29 @@ while True:
 
     contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     mask_filled = np.zeros_like(fgmask)
+    min_area = 80
     area_sum = 0
     for c in contours:
         if cv2.contourArea(c) > min_area:
             cv2.drawContours(mask_filled, [c], -1, 255, -1)
             area_sum += cv2.contourArea(c)
-    active_person_found = area_sum > active_area_sum
-    print(active_person_found, area_sum)
-
-    contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    mask_filled = np.zeros_like(fgmask)
-    min_area = 150        # Toleranz für kleine Konturen erhöht
-    area_sum = 0
-
-    for c in contours:
-        if cv2.contourArea(c) > min_area:
-            cv2.drawContours(mask_filled, [c], -1, 255, -1)
-            area_sum += cv2.contourArea(c)
-    active_person_found = area_sum > 350   # Gesamtfläche für „aktiv“ deutlich niedriger
 
     mask_filled = cv2.medianBlur(mask_filled, 3)
 
-    # --- Zeige letzte Maske, falls gerade keine erkannt ---
-    if active_person_found:
-        last_mask = mask_filled.copy()
-    elif last_mask is not None:
-        mask_filled = last_mask.copy()
+    # --- Beste Silhouette nur dann speichern, wenn sie maximal groß ist ---
+    if area_sum > max_area_sum and area_sum > 400:
+        max_area_sum = area_sum
+        master_mask = mask_filled.copy()
 
-    cv2.imshow("Person Mask", mask_filled)
+    # --- Anzeige: aktuelle Maske, sonst die "beste" ---
+    if area_sum > 120:
+        display_mask = mask_filled
+    elif master_mask is not None:
+        display_mask = master_mask
+    else:
+        display_mask = mask_filled
+
+    cv2.imshow("Person Mask", display_mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
