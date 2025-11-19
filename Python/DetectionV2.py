f50dc1f55e61e827aca72967cae141acfc80fc81
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2025-11-19 14:12:41 +0100

    Test File for Detection

--- Python/DetectionV2.py
+++ Python/DetectionV2.py
@@ -7,16 +7,21 @@ config = cam.create_preview_configuration(main={"size": (1280, 720)})
 cam.configure(config)
 cam.start()
 
-# History erhöht für weniger Geistereffekt
-fgbg = cv2.createBackgroundSubtractorKNN(history=70, dist2Threshold=350.0, detectShadows=False)
+fgbg = cv2.createBackgroundSubtractorKNN(history=100, dist2Threshold=400.0, detectShadows=False)
 
-# Größere Kernel für besseres Schließen von Lücken
 kernel_erode = np.ones((3, 3), np.uint8)
 kernel_dilate = np.ones((9, 9), np.uint8)
-kernel_close = np.ones((18, 18), np.uint8)  # Größer!
+kernel_close = np.ones((18, 18), np.uint8)
 
-master_mask = None
-max_area_sum = 0
+# Grid-Einstellungen: Teile das Bild in 3x3 Bereiche
+GRID_ROWS = 3
+GRID_COLS = 3
+
+# Speicher für jede Grid-Zelle
+grid_memory = {}
+for row in range(GRID_ROWS):
+    for col in range(GRID_COLS):
+        grid_memory[(row, col)] = None
 
 while True:
     frame = cam.capture_array()
@@ -30,40 +35,52 @@ while True:
     gray = cv2.equalizeHist(gray)
 
     fgmask = fgbg.apply(gray)
-
-    # Starkes Closing zuerst
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel_close)
-    # Mehr Dilation für Lückenschluss
     fgmask = cv2.dilate(fgmask, kernel_dilate, iterations=3)
     fgmask = cv2.erode(fgmask, kernel_erode, iterations=1)
-
     _, fgmask = cv2.threshold(fgmask, 127, 255, cv2.THRESH_BINARY)
 
+    # Konturen auf der gesamten Maske
     contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     mask_filled = np.zeros_like(fgmask)
-    min_area = 80
-    area_sum = 0
     for c in contours:
-        if cv2.contourArea(c) > min_area:
-            # cv2.FILLED statt -1 für garantierte Füllung
+        if cv2.contourArea(c) > 80:
             cv2.drawContours(mask_filled, [c], -1, 255, cv2.FILLED)
-            area_sum += cv2.contourArea(c)
 
     mask_filled = cv2.medianBlur(mask_filled, 3)
 
-    # Beste Silhouette speichern
-    if area_sum > max_area_sum and area_sum > 400:
-        max_area_sum = area_sum
-        master_mask = mask_filled.copy()
+    # Grid-Dimensionen berechnen
+    height, width = mask_filled.shape
+    cell_height = height // GRID_ROWS
+    cell_width = width // GRID_COLS
 
-    if area_sum > 120:
-        display_mask = mask_filled
-    elif master_mask is not None:
-        display_mask = master_mask
-    else:
-        display_mask = mask_filled
+    # Finale Maske mit Grid-Memory
+    final_mask = np.zeros_like(mask_filled)
 
-    cv2.imshow("Person Mask", display_mask)
+    for row in range(GRID_ROWS):
+        for col in range(GRID_COLS):
+            # Grid-Zelle definieren
+            y_start = row * cell_height
+            y_end = (row + 1) * cell_height if row < GRID_ROWS - 1 else height
+            x_start = col * cell_width
+            x_end = (col + 1) * cell_width if col < GRID_COLS - 1 else width
+
+            # Aktueller Bereich aus der Maske
+            cell_current = mask_filled[y_start:y_end, x_start:x_end]
+
+            # Prüfe ob Bewegung in dieser Zelle (weiße Pixel vorhanden)
+            motion_detected = np.count_nonzero(cell_current) > 100  # Schwellenwert anpassbar
+
+            if motion_detected:
+                # Aktualisiere Speicher für diese Zelle
+                grid_memory[(row, col)] = cell_current.copy()
+                final_mask[y_start:y_end, x_start:x_end] = cell_current
+            else:
+                # Keine Bewegung: Zeige gespeicherte Maske
+                if grid_memory[(row, col)] is not None:
+                    final_mask[y_start:y_end, x_start:x_end] = grid_memory[(row, col)]
+
+    cv2.imshow("Person Mask", final_mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
