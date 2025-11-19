4f717dc44bfd568ae874e8e318a6ac2dca1e3af2
M7rlon-von-Geo <m4nuel20xx@gmail.com>
2025-11-19 16:24:46 +0100

    bild

--- Uebertragung/main.py
+++ Uebertragung/main.py
@@ -5,17 +5,15 @@ from luma.core.render import canvas
 from luma.core.legacy import text
 from luma.core.legacy.font import proportional, LCD_FONT
 
-# 4 Module hintereinander
 serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, cascaded=4, block_orientation=-90)
-
-device.contrast(120)
+device = max7219(serial, cascaded=4, block_orientation=0)
+device.contrast(60)
 
 while True:
     with canvas(device) as draw:
-        # Jeder Buchstabe auf die entsprechende Matrix (8 Pixel Schritte)
-        text(draw, (0, 0), "M", fill="white", font=proportional(LCD_FONT))
-        text(draw, (8, 0), "A", fill="white", font=proportional(LCD_FONT))
-        text(draw, (16, 0), "N", fill="white", font=proportional(LCD_FONT))
-        text(draw, (24, 0), "U", fill="white", font=proportional(LCD_FONT))
+        # Zentrierung: x=2,y=1 für jedes Modul (optimale Werte für LCD_FONT)
+        text(draw, (2, 1), "M", fill="white", font=proportional(LCD_FONT))
+        text(draw, (10, 1), "A", fill="white", font=proportional(LCD_FONT))
+        text(draw, (18, 1), "N", fill="white", font=proportional(LCD_FONT))
+        text(draw, (26, 1), "U", fill="white", font=proportional(LCD_FONT))
     time.sleep(0.2)
