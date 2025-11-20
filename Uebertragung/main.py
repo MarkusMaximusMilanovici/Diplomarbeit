15f6bbba26bfa4e75f5d09cc974fc84e41df5958
M7rlon-von-Geo <m4nuel20xx@gmail.com>
2025-11-20 10:23:16 +0100

    tweaker

--- Uebertragung/main.py
+++ Uebertragung/main.py
@@ -6,24 +6,30 @@ from luma.core.legacy import text
 from luma.core.legacy.font import proportional, LCD_FONT
 
 serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, cascaded=8, block_orientation=-90)  # Zwei 4er-Module = 8 Module
-device.contrast(10)
+
+# Erstes 4er-Cluster, block_orientation -90
+device1 = max7219(serial, cascaded=4, block_orientation=-90)
+device1.contrast(10)
+
+# Zweites 4er-Cluster, block_orientation 90
+device2 = max7219(serial, cascaded=4, block_orientation=90)
+device2.contrast(10)
 
 lst = "Das Crazy euda wir fahrn zu WM "
 index = 0
 
 while True:
-    with canvas(device) as draw:
-        # Erstes 4er-Modul: Buchstaben auf Modul 0-3 platzieren
+    # Erstes Modul schreiben: Buchstaben 0-3 auf device1
+    with canvas(device1) as draw:
         text(draw, (2, 1), lst[(index + 0) % len(lst)], fill="white", font=proportional(LCD_FONT))
         text(draw, (10, 1), lst[(index + 1) % len(lst)], fill="white", font=proportional(LCD_FONT))
         text(draw, (18, 1), lst[(index + 2) % len(lst)], fill="white", font=proportional(LCD_FONT))
         text(draw, (26, 1), lst[(index + 3) % len(lst)], fill="white", font=proportional(LCD_FONT))
-
-        # Zweites 4er-Modul: Buchstaben auf Modul 4-7 platzieren
-        text(draw, (34, 1), lst[(index + 4) % len(lst)], fill="white", font=proportional(LCD_FONT))
-        text(draw, (42, 1), lst[(index + 5) % len(lst)], fill="white", font=proportional(LCD_FONT))
-        text(draw, (50, 1), lst[(index + 6) % len(lst)], fill="white", font=proportional(LCD_FONT))
-        text(draw, (58, 1), lst[(index + 7) % len(lst)], fill="white", font=proportional(LCD_FONT))
+    # Zweites Modul schreiben: Buchstaben 4-7 auf device2
+    with canvas(device2) as draw:
+        text(draw, (2, 1), lst[(index + 4) % len(lst)], fill="white", font=proportional(LCD_FONT))
+        text(draw, (10, 1), lst[(index + 5) % len(lst)], fill="white", font=proportional(LCD_FONT))
+        text(draw, (18, 1), lst[(index + 6) % len(lst)], fill="white", font=proportional(LCD_FONT))
+        text(draw, (26, 1), lst[(index + 7) % len(lst)], fill="white", font=proportional(LCD_FONT))
     time.sleep(0.5)
     index += 1
