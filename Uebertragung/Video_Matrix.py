1e06a46f0f9b359ae202945e8dfad21be4c2f05d
M7rlon-von-Geo <m4nuel20xx@gmail.com>
2025-11-26 15:16:20 +0100

    tweaker

--- Uebertragung/Video_Matrix.py
+++ Uebertragung/Video_Matrix.py
@@ -6,7 +6,7 @@ import cv2
 import numpy as np
 
 serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, cascaded=16, block_orientation=90)
+device = max7219(serial, cascaded=16, block_orientation=90, width = 32, height = 32)
 device.contrast(10)
 
 video_path = "output_32x32.mp4"
