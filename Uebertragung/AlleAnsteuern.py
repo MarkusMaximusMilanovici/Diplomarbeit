b752c85f32bc75b1de812595be2c9d8b6be2eaaf
M7rlon-von-Geo <m4nuel20xx@gmail.com>
2025-12-03 14:17:45 +0100

    tweaker

--- Uebertragung/AlleAnsteuern.py
+++ Uebertragung/AlleAnsteuern.py
@@ -1,31 +1,16 @@
 from luma.core.interface.serial import spi, noop
 from luma.led_matrix.device import max7219
-from luma.core.render import canvas
 from PIL import Image
-import time
 
-# Initialisierung: 4 Blöcke = 4 Geräte in Kaskade
+# Initialisierung: 4 Blöcke in Kaskade
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=0, rotate=0)
 
-# Helligkeit auf Maximum (0-255)
+# Helligkeit auf Maximum
 device.contrast(255)
 
-def all_on():
-    """Alle 16 LEDs (4 Blöcke) einschalten"""
-    img = Image.new('1', (32, 8), color=1)  # 4 Blöcke à 8x8 = 32x8
-    device.display(img)
-    print("Alle LEDs AN")
+# Alle LEDs einschalten
+img = Image.new('1', (32, 8), color=1)
+device.display(img)
 
-def all_off():
-    """Alle 16 LEDs (4 Blöcke) ausschalten"""
-    device.clear()
-    print("Alle LEDs AUS")
-
-# Beispiel-Nutzung
-if __name__ == "__main__":
-    while True:
-        all_on()
-        time.sleep(2)
-        all_off()
-        time.sleep(2)
+print("Alle 16 LEDs (4 Blöcke) sind jetzt AN mit voller Helligkeit!")
