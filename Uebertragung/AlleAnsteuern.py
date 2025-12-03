36eefebdd64868966c7b02ef89639d1cdfda6095
M7rlon-von-Geo <m4nuel20xx@gmail.com>
2025-12-03 14:20:05 +0100

    tweaker

--- Uebertragung/AlleAnsteuern.py
+++ Uebertragung/AlleAnsteuern.py
@@ -2,15 +2,18 @@ from luma.core.interface.serial import spi, noop
 from luma.led_matrix.device import max7219
 from PIL import Image
 
-# Initialisierung: 4 Blöcke in Kaskade
+# Initialisierung mit Reset
 serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, cascaded=4, block_orientation=0, rotate=0)
+device = max7219(serial, block_orientation=-90, width=32, height=32)
+
+# Device neu starten/clearen
+device.clear()
 
 # Helligkeit auf Maximum
 device.contrast(255)
 
-# Alle LEDs einschalten
-img = Image.new('1', (32, 8), color=1)
+# Alle LEDs einschalten - RICHTIGE Größe für 32x32!
+img = Image.new('1', (32, 32), color=1)
 device.display(img)
 
-print("Alle 16 LEDs (4 Blöcke) sind jetzt AN mit voller Helligkeit!")
+print("Alle 16 Blöcke (32x32 LEDs) sind jetzt AN mit voller Helligkeit!")
