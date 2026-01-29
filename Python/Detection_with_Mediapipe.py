a8400dc1cb520647ce59480c5b28bcac7aef1d7d
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2026-01-29 09:13:07 +0100

    wieder 2 hände

--- Python/Detection_with_Mediapipe.py
+++ Python/Detection_with_Mediapipe.py
@@ -58,7 +58,7 @@ segmenter = mp_selfie.SelfieSegmentation(model_selection=1)
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     static_image_mode=False,
-    max_num_hands=4,
+    max_num_hands=2,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.6
 )
