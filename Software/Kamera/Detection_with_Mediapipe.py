6e099e37d84132f7f9f37f38a3b89b3b3d58d4f8
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2026-03-01 19:37:51 +0100

    der bisherige Stand. die Sihlouette is gut aber die Hände sind noch immer problematisch. sie werden angezeigt und falls die KI nachlässt helfen die Lanmarklinien aber die Finger verschwimmen oft miteinander aber im großen und ganzen funktioniert es gut

--- Software/Kamera/Detection_with_Mediapipe.py
+++ Software/Kamera/Detection_with_Mediapipe.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import mediapipe as mp
-import Software.LED_Matrix.ImagetoMatrix as ImagetoMatrix
+import ImagetoMatrix
 
 # ============================================================
 # Kamera-Auswahl: Raspberry Pi (PiCamera2) ODER Laptop (cv2.VideoCapture)
