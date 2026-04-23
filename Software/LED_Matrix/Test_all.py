3965f27a56e81f28fe2f39988d83daa7c307d09e
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2026-04-23 10:30:53 +0200

    Die letzten Commits vor der Abgabe

--- Software/LED_Matrix/Test_all.py
+++ Software/LED_Matrix/Test_all.py
@@ -18,7 +18,6 @@ device.contrast(10)
 # ALLE LEDs AN
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, fill="white")
-
-# Optional: dauerhaft an lassen
-while True:
+    time.sleep(1)
+    draw.rectangle(device.bounding_box, fill="black")
     time.sleep(1)
