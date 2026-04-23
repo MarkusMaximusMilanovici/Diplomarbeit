8838b753c88dcb2ac6f665d51804acfb72338084
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2026-04-23 10:22:18 +0200

    Die letzten Commits vor der Abgabe

--- Software/LED_Matrix/Test_all.py
+++ Software/LED_Matrix/Test_all.py
@@ -1,16 +1,25 @@
 from luma.core.interface.serial import spi, noop
 from luma.led_matrix.device import max7219
 from luma.core.render import canvas
+import time
 
 # SPI initialisieren
 serial = spi(port=0, device=0, gpio=noop())
 
-# Device (deine Matrix)
+# Device initialisieren
 device = max7219(serial, block_orientation=-90, width=32, height=48)
 
-# Helligkeit (0–255)
+# Helligkeit einstellen
 device.contrast(10)
 
-# Alle LEDs einschalten
-with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="white", fill="white")
+while True:
+    # ALLE LEDs AN
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, fill="white")
+
+    time.sleep(1)  # 1 Sekunde an
+
+    # ALLE LEDs AUS
+    device.clear()
+
+    time.sleep(1)  # 1 Sekunde aus
