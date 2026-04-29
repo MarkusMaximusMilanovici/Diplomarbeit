88e26905d74384acd4083f047cca18717bb1699d
MarkMaximusMilanovici <milanovicirobert@gmail.com>
2026-04-29 14:14:32 +0200

    Die letzten Commits vor der Abgabe

--- Software/LED_Matrix/Test_all.py
+++ Software/LED_Matrix/Test_all.py
@@ -1,24 +1,28 @@
-from luma.core.interface.serial import spi, noop
 from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 import time
 
-# 👉 HIER nur Anzahl deiner Module ändern
-NUM_MODULES = 32   # z.B. 18 Module
+# Anzahl deiner Matrizen (z.B. 4 für ein Modul)
+NUM_MATRICES = 4
 
-# SPI
+# SPI Setup
 serial = spi(port=0, device=0, gpio=noop())
 
-# Device (nur Anzahl zählt!)
-device = max7219(serial, cascaded=NUM_MODULES)
+# Device
+device = max7219(serial, cascaded=NUM_MATRICES, block_orientation=-90)
 
-# Helligkeit
-device.contrast(10)
+# 🔥 SEHR NIEDRIGE HELLIGKEIT (0–255)
+device.contrast(1)   # probier auch 0 oder 2
 
-# ALLE LEDs AN
+# Größe berechnen
+WIDTH = NUM_MATRICES * 8
+HEIGHT = 8
+
+# Alle LEDs AN
 with canvas(device) as draw:
-    while True:
-        draw.rectangle(device.bounding_box, fill="white")
-        time.sleep(1)
-        device.clear()
-        time.sleep(1)
+    draw.rectangle((0, 0, WIDTH - 1, HEIGHT - 1), fill="white")
+
+# Programm läuft einfach weiter (damit es nicht beendet wird)
+while True:
+    time.sleep(1)
